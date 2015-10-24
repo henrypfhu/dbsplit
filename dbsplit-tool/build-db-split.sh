3fb6b2b56df3eec700e7376604bbae4ad88041df
robert <robert@robertdeMacBook-Pro.local>
2015-10-24 20:54:17 +0800

    Remove the tr because it prevent running sql in mac.

--- dbsplit-tool/build-db-split.sh
+++ dbsplit-tool/build-db-split.sh
@@ -35,7 +35,7 @@ build_db() {
 
     echo "info: building instance $inst db $db db no $db_no table $table_no"    
     
-    sql_command="sed 's/"'$I'"/$table_no/g' $table_sql_file | tr -t '\n' '\n'"
+    sql_command="sed 's/"'$I'"/$table_no/g' $table_sql_file "
     sql_create_table=`eval "$sql_command"`
     
     if [[ $debug = 'TRUE' ]]; then
